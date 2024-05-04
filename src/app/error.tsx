08d75c0dd1ed2e2f54f100da2f78bcf7f9686183
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-05-04 17:02:24 +0330

    Add: added Routr for website section - #2

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Error(){
+    return(
+        <div className="text-6xl text-red-800  flex items-center justify-center">
+            Error
+        </div>
+    )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return(
+        <div className=" flex text-3xl justify-center items-center text-red-700">
+            Not Found !
+        </div>
+    )
+}
