5e5e044340aba5a65aece8f92200dfa5f11bf0dc
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-05-09 11:19:10 +0330

    Add: added global type for components - #8

---
+++ src/app/_components/types/component-base.type.ts
@@ -0,0 +1,10 @@
+
+import { Size } from "./size,type"
+import { Variant } from "./variant.type"
+
+ export type ComponentBase = {
+    isDisabled?:boolean,
+    clasName?:string,
+    variant?:Variant ,
+    size?:Size;
+ }
