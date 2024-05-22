711c0902918e7ac344706796249142fbd42bf0db
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-05-22 12:16:16 +0330

    Add: added feature component - #20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import { CourseSummary } from "@/types/course-summery.interface";
 import {HomeHeroSection} from "./_components/home-hero-section/home-hero-section";
 import { CourseCardList } from "./(courses)/_component/course-card-list";
+import Feature from "./_components/feature/feature";
+import { homeFeatures } from "@/data/home-feature";
 
 
 
@@ -22,6 +24,13 @@ export default async function Home() {
   return (
     <>
      <HomeHeroSection/>
+     <section className="dark:bg-base-75 mt-10 px-20 container">
+          <div className="container py-10 flex flex-col lg:flex-row gap-10 xl:gap-5">
+              {homeFeatures.map((feature) => (
+                  <Feature key={`feature-${feature.title}`} feature={feature} />
+              ))}
+          </div>
+      </section>
      <section className="container px-20  pt-20">
         <div className="text-center xl:text-right">
             <h2 className="text-2xl font-extrabold">
