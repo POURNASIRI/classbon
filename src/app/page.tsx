876a0ae0dfb67bb99330ff5858c20c9003ecd6bf
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-05-23 22:12:38 +0330

    Add: added get newest blog post function  - #21

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import {HomeHeroSection} from "./_components/home-hero-section/home-hero-section
 import { CourseCardList } from "./(courses)/_component/course-card-list";
 import Feature from "./_components/feature/feature";
 import { homeFeatures } from "@/data/home-feature";
+import { IconArrowLeftFill } from "./_components/icons/icons";
+import { Button } from "./_components/button";
 
 
 
@@ -16,11 +18,23 @@ async function getNewestCourses(count:number):Promise<CourseSummary[]>{
       });
     return res.json()
 }
+async function getNewestPosts(count:number):Promise<CourseSummary[]>{
+    const res  = await fetch(`https://api.classbon.com/api/blog/newest/${count}`,
+      {
+        next:{
+          revalidate: 24 * 60 * 60
+        }
+      });
+    return res.json()
+}
 
 
 export default async function Home() {
 
-   const newestCourses = await getNewestCourses(4 )
+   const newestCoursesData =  getNewestCourses(4)
+   const newestBlogPostsData =  getNewestCourses(4)
+   const [newestCourses,newestBlogPosts] = await Promise.all([newestCoursesData,newestBlogPostsData])
+   console.log(newestBlogPosts)
   return (
     <>
      <HomeHeroSection/>
@@ -42,6 +56,45 @@ export default async function Home() {
         </div>
         <CourseCardList courses={newestCourses} />
         </section>
+        <section className="px-2 my-40">
+                {/* <div className="sticky top-0 pt-0 text-center"> */}
+                <div className="relative pt-0 text-center">
+                    <div className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+
+                    <h2
+                        lang="en"
+                        className="gradient leading-[1.3] relative z-10 mx-auto inline-block text-[clamp(2rem,6vw,5.5rem)] font-black"
+                    >
+                        ReactJs & Next.js
+                    </h2>
+                    <p className="text-base-content/70  relative z-[2] py-4 m-auto md:text-3xl max-w-5xl font-light !leading-[1.7]">
+                        ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و
+                        یکه‌تاز دنیای وب! پیشرفته‌ترین مباحث رو اینجا می تونی
+                        پیدا کنی. پس همین الان یادگیری رو شروع کن ما هم از
+                        ابتدای مسیر با آموزش‌های تخصصی و کاملاً کاربردی کنارت
+                        هستیم.
+                    </p>
+                    <div className="flex flex-col lg:flex-row items-center gap-3 justify-center">
+                        <Button
+                            variant="primary"
+                            size="large"
+                            className="mt-7"
+                            animatedIcon={true}
+                        >
+                            دوره‌های ری اکت و نکست‌ جی‌اس
+                            <IconArrowLeftFill fill="currentColor" />
+                        </Button>
+                        <Button
+                            variant="neutral"
+                            size="large"
+                            className="mt-7"
+                            animatedIcon={true}
+                        >
+                            مقالات ری اکت و نکست‌ جی‌اس
+                        </Button>
+                    </div>
+                </div>
+            </section>
     </>
   );
 }
