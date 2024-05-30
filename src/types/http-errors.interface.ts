157a843ba27bb839454467e5289e6ca6157ab8d7
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-05-30 11:42:36 +0330

    Add : added http errors type - #35

---
+++ src/types/http-errors.interface.ts
@@ -0,0 +1,24 @@
+interface Problem {
+    title:string;
+    statusCode: number;
+    details?:string;
+    errors?:Record<string, string[]>
+}
+
+
+interface BadRequestError extends Problem {} //400
+interface UnauthorizedError extends Problem {} //403 token is valid but user cannot accsess specific section => 401  tokrn not valid 
+interface ValidationError extends Problem {} // 400 but have errors property 
+interface NotFoundError extends Problem {} // 404 api not found
+interface UnhandledException extends Problem {} // 500 
+interface NetworkError extends Problem {} //
+
+export type {
+    Problem,
+    BadRequestError,
+    UnauthorizedError,
+    ValidationError,
+    NotFoundError,
+    UnhandledException,
+    NetworkError
+};
