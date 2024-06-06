fa25b5ff0675ac4942b48210fe3b4c69d7efb650
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-06-06 10:47:14 +0330

    Add:added course chapter component - #50

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -8,6 +8,8 @@ import { Tab } from "@/types/tab.type";
 import { Accordion as AccordionType } from "@/types/accordion.types";
 import { Accordion } from "@/app/_components/accordion";
 import CourseComments from "./_components/comments/course-comments";
+import { CourseChapter } from "@/types/course-chapter.interface";
+import { CourseCurriculum } from "./_components/curriculum";
 
 
 
@@ -25,11 +27,19 @@ async function getCourse(slug: string): Promise<CourseDetails> {
     const res = await fetch(`${API_URL}/courses/${slug}`);
     return res.json();
 }
+
+async function getCurriculum(slug:string): Promise<CourseChapter[]>{
+    const res = await fetch(`https://api.classbon.com/api/courses/${slug}/curriculum`)
+    return res.json()
+}
+
 export default async function CourseDetails({params}: {params: {slug: string}}) {
     const {slug} = params;
-    const courseData = await getCourse(slug);
+    const courseData =  getCourse(slug);
+    const courseCurriculum = getCurriculum(slug);
+    const [course,courseCrriculum] = await Promise.all([courseData,courseCurriculum])
 
-    const faqs: AccordionType[] = courseData.frequentlyAskedQuestions.map(
+    const faqs: AccordionType[] = course.frequentlyAskedQuestions.map(
         faq => ({
             id: faq.id,
             title: faq.question,
@@ -42,7 +52,7 @@ export default async function CourseDetails({params}: {params: {slug: string}}) 
     const tabs: Tab[] = [
         {
             label: "مشخصات دوره",
-            content: courseData.description,
+            content: course.description,
         },
         {
             label: "دیدگاه‌ها و پرسش",
@@ -58,21 +68,26 @@ export default async function CourseDetails({params}: {params: {slug: string}}) 
             <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
             <div className="col-span-10 xl:col-span-7">
                 <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
-                    {courseData.title}
+                    {course.title}
                 </h1>
                 <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
-                    {courseData.subTitle}
+                    {course.subTitle}
                 </h2>
 
                 <div className=" mt-5">Video Player Component</div>
             </div>
             <div className="col-span-10 xl:col-span-3">
-               <CourseAside {...courseData}/>
+               <CourseAside {...course}/>
             </div>
             <div className="col-span-10 xl:col-span-6">
                 <Tabs tabs={tabs}/>
             </div>
-            <div className="col-span-10 xl:col-span-4 bg-warning"></div>
+            <div className=" col-span-10 xl:col-span-4 ">
+            <div className="sticky top-5">
+                <h2 className="mb-5 text-xl">سرفصل های دوره</h2>
+                <CourseCurriculum data={courseCrriculum}/>
+            </div>
+        </div>
         </div>
     )
 }
