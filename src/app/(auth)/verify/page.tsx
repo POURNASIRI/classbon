f0f2fc0efde7bcf57446715a94a9fdd1dc104701
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-06-09 15:47:15 +0330

    Add:added remplate for authentication - #56

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,8 +1,9 @@
-export default async function Verify(){
+export default async function Verification(){
     
+ 
     return(
-        <div>
-            Verify
+        <div className="text-5xl flex justify-center items-center">
+            <h1>Verification</h1>
         </div>
     )
 }
--- src/app/(auth)/veryfi/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Verification(){
-    
- 
-    return(
-        <div className="text-5xl flex justify-center items-center">
-            <h1>Verification</h1>
-        </div>
-    )
-}
