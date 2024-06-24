d4503c2a1ecc1db768279f78d749a462dcbb63aa
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-06-25 00:22:22 +0330

    Add:added timer progres component - #74

--- src/app/(auth)/verify/components/verification-form.tsx
+++ src/app/(auth)/verify/components/verification-form.tsx
@@ -1,12 +1,11 @@
-'use client'
+'use client';
 
-
-import AuthCode from "@/app/_components/auth-code/auth-code";
-import { AuthCodeRef } from "@/app/_components/auth-code/auth-code.types";
-import { Button } from "@/app/_components/button/button";
-import { Timer } from "@/app/_components/timer/timer";
-import Link from "next/link";
-import { useRef } from "react";
+import AuthCode from '@/app/_components/auth-code/auth-code';
+import { AuthCodeRef } from '@/app/_components/auth-code/auth-code.types';
+import { Button } from '@/app/_components/button/button';
+import { Timer } from '@/app/_components/timer/timer';
+import Link from 'next/link';
+import { useRef } from 'react';
 
 const getTwoMinutesFromNow = () => {
   const time = new Date();
@@ -15,46 +14,35 @@ const getTwoMinutesFromNow = () => {
 };
 
 const VerificationForm = () => {
-
-  const authCodeRef = useRef<AuthCodeRef>(null)
+  const authCodeRef = useRef<AuthCodeRef>(null);
 
   return (
     <>
-      <h5 className="text-2xl">کد تایید</h5>
-      <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
-      <form
-        className="flex flex-col gap-6 mt-10 flex-1"
-      > 
-        <AuthCode 
-        ref={authCodeRef}
-        className="mt-10" onChange={value=>{
-            console.log(value)
-        }}/>
+      <h5 className='text-2xl'>کد تایید</h5>
+      <p className='mt-2'>دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
+      <form className='flex flex-col gap-6 mt-10 flex-1'>
+        <AuthCode
+          ref={authCodeRef}
+          className='mt-10'
+          onChange={(value) => {
+            console.log(value);
+          }}
+        />
 
-
-        <Timer expiryTimestamp={getTwoMinutesFromNow()}/>
-        <Button
-          isLink={true}
-          
-        >
-          ارسال مجدد کد تایید
-        </Button>
-        <Button
-          type="submit"
-          variant="primary"
-        
-        >
+        <Timer expiryTimestamp={getTwoMinutesFromNow()} showDays={false} showHours={false} />
+        <Button isLink={true}>ارسال مجدد کد تایید</Button>
+        <Button type='submit' variant='primary'>
           تایید و ادامه
         </Button>
 
-        <div className="flex items-start gap-1 justify-center mt-auto">
+        <div className='flex items-start gap-1 justify-center mt-auto'>
           <span>برای اصلاح شماره موبایل</span>
-          <Link href="/signin">اینجا</Link>
+          <Link href='/signin'>اینجا</Link>
           <span>کلیک کنید</span>
         </div>
       </form>
     </>
-  )
-}
+  );
+};
 
 export default VerificationForm;
