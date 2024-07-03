291d99a8c764665be2ba6af84f3127370f22f00f
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-07-03 11:07:10 +0330

    Add: added zod validation in server action - #79

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,8 +1,19 @@
 'use server'
 
+import { signInSchema } from "@/app/(auth)/signin/types/signin-schema"
 import { redirect } from "next/navigation"
 
 export async function signInAction(mobile:string){
+
+    const validateData = signInSchema.safeParse({
+        mobile
+    })
+
+    if(validateData.success){
+        console.log(mobile)
+    }else{
+        console.log('error')
+    }
     console.log(mobile)
     redirect('/')
 }
