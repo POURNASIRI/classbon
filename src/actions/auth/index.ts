9d949e01b93426f0662b123aaaf40b1b47f336c9
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-07-04 23:38:22 +0330

    Add:added server wrapper and use start trasition - #81

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,28 +1,31 @@
-'use server'
+"use server";
 
-import { signInSchema } from "@/app/(auth)/signin/types/signin-schema"
-import { redirect } from "next/navigation"
 
-export async function signInAction(formState:{message:string}, formData:FormData){
+import { OperationResult } from "@/types/operation-result";
+import { redirect } from "next/navigation";
+import { serverActionWrapper } from "../server-action-wrapper";
+import { createData } from "@/core/http-service/http-service";
+import { SignIn } from "@/app/(auth)/signin/types/signin.types";
 
-    const mobile = formData.get('mobile')
-    const validateData = signInSchema.safeParse({
-        mobile
-    })
+export async function signInAction(
+    formState: OperationResult<string> | null,
+    formData: FormData
+) {
+     const mobile = formData.get("mobile") as string;
+    // const validatedData = signInSchema.safeParse({
+    //     mobile,
+    // });
 
-    if(validateData.success){
-        return {
-            message:"شماره موبایل صحیح است"
-        }
-    }else{
-        try {
-            throw 'خطا در برقراری ارتباط با سرور'
-        } catch (error) {
-            return{
-                message: error as string
-            }
-        }
-    }
-    console.log(mobile)
-    redirect('/')
+    // if (!validatedData.success) {
+    //     return {
+    //         message: "خطا در فرمت موبایل",
+    //     };
+    // } else {
+        return serverActionWrapper(
+            async () =>
+                await createData<SignIn, string>("/signin", {
+                    mobile,
+                })
+        );
+    // }
 }
--- src/app/(auth)/signin/components/signin-form.tsx
+++ src/app/(auth)/signin/components/signin-form.tsx
@@ -1,65 +1,60 @@
 "use client";
 
 import { Button } from "@/app/_components/button/button";
+
 import { SignIn } from "../types/signin.types";
 import { useForm } from "react-hook-form";
 import { TextInput } from "@/app/_components/form-input";
-// import { useSingIn } from "../api/signin";
 import { useRouter } from "next/navigation";
-import { useNotificationStore } from "@/store/notification.store";
-// import { useEffect } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signInSchema } from "../types/signin-schema";
-import { signInAction } from "@/actions/auth";
-
-// for send data from client to server we can use useFormState react hook from react dom 
-
 import { useFormState } from "react-dom";
-
-
+import { useEffect, useTransition } from "react";
+import { signInSchema } from "../types/signin-schema";
+import { useNotificationStore } from "@/store/notification.store";
+import { signInAction } from "@/actions/auth";
 
 const SignInForm = () => {
     const {
         register,
         handleSubmit,
-        getValues,
         formState: { errors },
+        getValues,
     } = useForm<SignIn>({
-        resolver:zodResolver(signInSchema)
+        resolver: zodResolver(signInSchema),
     });
 
-    const router = useRouter()
+    const [formState, action] = useFormState(signInAction, null);
+    const [isPending, startTransition] = useTransition();
 
-    const showNotification  = useNotificationStore(state => state.showNotification)
+    const router = useRouter();
 
-    // const signIn = useSingIn({
-    //     onSuccess() {
-    //         router.push(`/verify?mobile = ${getValues('mobile')}`)
-    //         showNotification({
-    //             message:"کد تایید به شماره شما ارسال شد",
-    //             type:"success"
-    //         })
-    //     },
-    // })
+    const showNotification = useNotificationStore(
+        (state) => state.showNotification
+    );
 
+    useEffect(() => {
+        if (formState && !formState.isSuccess && formState.error) {
+            showNotification({
+                message: formState.error?.detail!,
+                type: "error",
+            });
+        } else if (formState && formState.isSuccess) {
+            router.push(`/verify?mobile=${getValues("mobile")}`);
+            showNotification({
+                message: "کد تایید به شماره شما ارسال شد",
+                type: "info",
+            });
+            console.log(formState.response);
+        }
+    }, [formState, showNotification, router, getValues]);
 
-
-    //  useForm State
-    const[formState,action] =  useFormState(signInAction,{message:''})
-
-    const onSubmit = (data:any) => {
-        const formData = new FormData()
-        formData.append('mobile',data.mobile)
-        // because we use react hook form we must call our action here
-        action(formData)
-        // signIn.submit(data)
-    }
-    
-    
-    // note : when we use useNotification hook to prevent rerender component to change any our store
-    // property we must use like this:
-    // in this way the component just rerender when  showNotification has been change
-
+    const onSubmit = (data: SignIn) => {
+        const formData = new FormData();
+        formData.append("mobile", data.mobile);
+        startTransition(async() => {
+            await action(formData);
+        });
+    };
 
     return (
         <>
@@ -67,30 +62,17 @@ const SignInForm = () => {
             <p className="mt-2">
                 دنیای شگفت انگیز برنامه نویسی در انتظار شماست!
             </p>
-            <form  className="flex flex-col gap-6 mt-16" onSubmit={handleSubmit(onSubmit)}>
+            <form
+                className="flex flex-col gap-6 mt-16"
+                onSubmit={handleSubmit(onSubmit)}
+            >
                 <TextInput<SignIn>
-                    // this property for register
-                  register={register}
-                  // this property for name
-                  name={"mobile"}
-                  // and rules property is an object for our validation
-                //   use zod schema
-                //   rules={{
-                //     required: 'شماره موبایل الزامی است',
-                //     maxLength: {
-                //         value: 11,
-                //         message: 'شماره موبایل باید 11 رقم باشد'
-                //       },
-                //       minLength: {
-                //         value: 11,
-                //         message: 'شماره موبایل باید 11 رقم باشد'
-                //       }
-                //   }}
-                  // this property for gry errors
-                  errors={errors}
+                    register={register}
+                    name={"mobile"}
+                    errors={errors}
                 />
 
-                <Button type="submit"  variant="primary">
+                <Button type="submit" variant="primary" isLoading={isPending}>
                     تایید و دریافت کد
                 </Button>
             </form>
